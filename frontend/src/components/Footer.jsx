a9b3efdb3bbe2363b866041f5851a948014de7e0
dutaramadhan <duta2711@gmail.com>
2024-07-27 12:55:34 +0700

    fix: edit footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <main className="flex-grow overflow-y-auto">
       </main>
       <footer className="bg-blue-500 py-[20px] text-black px-[5%] md:text-[18px] fixed bottom-0 left-0 right-0">
-        <h1 className="text-center">2024 &copy; KKN PPM UGM - Warna-Warni Wongsorejo</h1>
+        <h1 className="text-center">2024 &copy; KKN PPM UGM - Warna-Warni Wongsorejo X MI Tarbiyatus Sibyan</h1>
       </footer>
     </div>
   );
