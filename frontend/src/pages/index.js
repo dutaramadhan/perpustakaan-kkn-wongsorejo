7dcd261e56c3a20000bebfb8d1e8cd68378fe63d
dutaramadhan <duta2711@gmail.com>
2024-07-27 10:39:07 +0700

    feat: add seo

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -9,7 +9,6 @@ import { toast } from 'react-toastify';
 export default function Home() {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
     useEffect(() => {
       const fetchBooks = async () => {
@@ -35,18 +34,34 @@ export default function Home() {
   }
 
   return (
-    <main>
-      <Navbar Text={"Perpustakaan Digital"}/>
-      <div className="flex-grow flex flex-col items-center py-20">
-        <div className="w-full flex-grow overflow-auto">
-          <div className="grid grid-cols-2 gap-5 p-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7">
-            {books.map((book) => (
-              <BookCard key={book._id} book={book} href={`/books/${book._id}`}/>
-            ))}
+    <>
+      <Head>
+        <title>Perpustakaan Digital KKN PPM UGM Wongsorejo 2024</title>
+        <meta name="description" content="Selamat datang di Perpustakaan Digital KKN UGM Wongsorejo 2024. Temukan berbagai buku dan penulis di sini." />
+        <meta name="keywords" content="perpustakaan digital, KKN UGM, Wongsorejo, buku, penulis, membaca" />
+        <meta name="author" content="KKN UGM Wongsorejo 2024" />
+        <meta property="og:title" content="Perpustakaan Digital KKN UGM Wongsorejo 2024" />
+        <meta property="og:description" content="Selamat datang di Perpustakaan Digital KKN UGM Wongsorejo 2024. Temukan berbagai buku dan penulis di sini." />
+        <meta property="og:image" content="/images/library-og-image.jpg" />
+        <meta property="og:url" content="https://www.perpustakaan-wongsorejo-ugm.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Perpustakaan Digital KKN UGM Wongsorejo 2024" />
+        <meta name="twitter:description" content="Selamat datang di Perpustakaan Digital KKN UGM Wongsorejo 2024. Temukan berbagai buku dan penulis di sini." />
+        <meta name="twitter:image" content="/images/library-twitter-image.jpg" />
+      </Head>
+      <main>
+        <Navbar Text={"Perpustakaan Digital"}/>
+        <div className="flex-grow flex flex-col items-center py-20">
+          <div className="w-full flex-grow overflow-auto">
+            <div className="grid grid-cols-2 gap-5 p-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7">
+              {books.map((book) => (
+                <BookCard key={book._id} book={book} href={`/books/${book._id}`}/>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <Footer />
-    </main>
+        <Footer />
+      </main>
+    </>
   );
 }
