4d272e5c62b3f9579ee5aaa48978180eeba4145d
dutaramadhan <duta2711@gmail.com>
2024-07-27 10:45:44 +0700

    fix: head error

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import axios from 'axios';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
